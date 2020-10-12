0b9648a119e1bbdf6fd5d19c7fa67b36f4cdd938
Mateusz <matt.borowczyk@gmail.com>
2020-10-12 20:42:45 +0200

    activating event lsiteners module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 require('./style.css');
 import {Project, defaultProject} from './projects.js';
 import {toDoTask} from './todos.js';
-//import {eventListeners} from './DOMs'
+import {eventListeners} from './DOMs'
