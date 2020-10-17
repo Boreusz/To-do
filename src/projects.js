038fc757ea670d5cbe17af84220e4f44846bd8e9
Mateusz <matt.borowczyk@gmail.com>
2020-10-17 20:54:12 +0200

    adding counter IFEE for dealing with projects IDs

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,22 @@
-const projectHolder = []; //optional, find if this variable is needed
-const Project = (name, color = "black") => {
+const projectHolder = []; //opt(ional, find if this variable is needed
+const projectcounter = (() => {
+    let value = 0;
+    const getValue = () => value;
+    const incrementValue = () => {
+        value++;
+    }
+    return {
+        getValue,
+        incrementValue
+    }
+})() // id for projects
+const Project = (name, color = "black", id = projectHolder.length()) => {
     const toDoList = [];
     const getName = () => name;
     const changeName = (newName) => name = newName;
     const getColor = () => color;
     const changeColor = (newColor) => color = newColor;
+    const getID = () => id;
     const addTask = (task) => toDoList.push(task);
     const deleteTask = (taskIndex) => toDoList.splice(taskIndex, 1);
     return{
@@ -12,14 +24,17 @@ const Project = (name, color = "black") => {
         changeName,
         getColor,
         changeColor,
+        getID,
         addTask,
         deleteTask
     }
 }
-const defaultProject = Project("Default");
+const defaultProject = Project("Default", "black", projectcounter.getValue());
 projectHolder.push(defaultProject);
+projectcounter.incrementValue();
 export {
     Project,
+    projectcounter,
     projectHolder,
     defaultProject,
 }
