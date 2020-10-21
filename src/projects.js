56797696a2d439ebaf26a85947664114f2950887
Mateusz <matt.borowczyk@gmail.com>
2020-10-21 23:59:32 +0200

    Task counter for IDs added

--- src/projects.js
+++ src/projects.js
@@ -12,21 +12,28 @@ const projectcounter = (() => {
 })() // id for projects
 const Project = (name, color = "black", id = projectHolder.length()) => {
     const toDoList = [];
+    let taskCounter = 0;
+    const giveTaskID = () => taskCounter;
     const getName = () => name;
     const changeName = (newName) => name = newName;
     const getColor = () => color;
     const changeColor = (newColor) => color = newColor;
     const getID = () => id;
-    const addTask = (task) => toDoList.push(task);
+    const addTask = (task) => {
+        toDoList.push(task);
+        taskCounter++;
+    }
     const deleteTask = (taskIndex) => toDoList.splice(taskIndex, 1);
     return{
+        toDoList,
         getName,
         changeName,
         getColor,
         changeColor,
         getID,
         addTask,
-        deleteTask
+        deleteTask,
+        giveTaskID
     }
 }
 const defaultProject = Project("Default", "black", projectcounter.getValue());
